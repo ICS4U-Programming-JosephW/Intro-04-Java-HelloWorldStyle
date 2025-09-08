68d627d678ca357f73a6bf02f8d3c10dfd09575e
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-08 15:08:29 +0000

    Finished linter and added style to comments.

--- HelloWorldStyle.java
+++ HelloWorldStyle.java
@@ -1,15 +1,35 @@
 /**
-* Explain what your program does.
+* This program adds more style and
+* comments to the greeting program.
 *
 * @author  Joseph Wondimagnehu
-* @version 1.0
-* @since   Year-Month-Day
+* @version 2.0
+* @since   2025-09-08
 */
+public final class HelloWorldStyle {
+  /**
+   * Creates a private constructor
+   * to satisfy the style checker.
+   * @exception IllegalStateException for the utility class.
+   * @see IllegalStateException
+   */
+  private HelloWorldStyle() {
+    // Prevents illegal states.
+    throw new IllegalStateException("Utility class.");
+  }
 
-// Creates the container for the program.
-class HelloWorldStyle {
-  // Defines the entry point of the program.
-  public static void main(String[] args) {
-    System.out.println("jdfs");
+  /**
+   * Defines the entry point of the program.
+   *
+   * @param args unused, for now.
+   */
+  public static void main(final String[] args) {
+    // Greets Beni.
+    System.out.println("\nHello, Beni! With style!");
+    // Outputs an additional statement.
+    System.out.println(
+      "Java is the superior language!"
+      + " In my opinion, of course.\n"
+    );
   }
 }
